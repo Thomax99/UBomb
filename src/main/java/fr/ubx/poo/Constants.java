a018289554b3a20f9ca20a488137a6057978f2fc
Thomax99 <thomas.morin@etu.u-bordeaux.fr>
2021-01-07 11:41:35 +0100

    changement de l'architecture de l'aléatoire

--- src/main/java/fr/ubx/poo/Constants.java
+++ src/main/java/fr/ubx/poo/Constants.java
@@ -9,4 +9,11 @@ package fr.ubx.poo;
  */
 public final class Constants {
     public static long secondInnanoSec = 1000000000L ; // storage of the number on nanoseconds in one second
+
+    // default values for the random generation
+    public static int defaultMinWidthValue = 12, defaultMaxWidthValue = 30, defaultMinHeightValue = 10, defaultMaxHeightValue = 25 ;
+    public static double defaultEmptyProba = 0.73, defaultBoxProba = 0.07, defaultMonsterProba = 0.02, defaultStoneProba = 0.07,
+                        defaultTreeProba = 0.02, defaultNumberIncProba = 0.012, defaultNumberDecProba = 0.012, defaultRangeIncProba = 0.015,
+                        defaultRangeDecProba = 0.02, defaultHeartProba = 0.01, defaultLandminerProba = 0.017, defaultScarecrowProba = 0.009 ;
+    public static String propertiesFileName = "config.properties" ;
 }
--- src/main/java/fr/ubx/poo/game/WorldBuilder.java
+++ src/main/java/fr/ubx/poo/game/WorldBuilder.java
@@ -31,96 +31,13 @@ public class WorldBuilder {
         return builder.grid;
     }
     /**
-     * THIS IS NOT A PORTABLE IMPLEMENTATION
      * @param path the path of the file config.properties in which the probabilities are registered
      * @param level the number of the current level
      * @param nb_level_max the number maximum of the level
      * @return a randomly generate world
      */
     public static WorldEntity[][] randomBuild(String path, int level, int nb_level_max) {
-        int width = (int) (Math.random()*18 +12), height = (int) (Math.random()*15 +10) ; // the Dimension of a game is at least a square of 12*10 and at more a rectangle of 30*25
-        WorldEntity[][] raw = new WorldEntity[height][width] ; //we generate the array of entities
-        boolean isFirstLevel = (level == 1) ; //useful to know if we have to generate a position of the player and a previousDoor
-        boolean isLastLevel = (level == nb_level_max) ; //useful to know if we have to generate a princess, a nextDoor and a key
-
-        int x, y ;
-
-        if (isFirstLevel){
-            //we need to compute a player position
-            x = (int) (Math.random()*width) ; y = (int) (Math.random()*height) ;
-            raw[y][x] = WorldEntity.Player ;
-            //here we're going to make empty the adjacents cases of the player position
-            if (x - 1 >= 0) raw[y][x-1] = WorldEntity.Empty ;
-            if (x + 1 < width) raw[y][x+1] = WorldEntity.Empty ;
-            if (y - 1 >= 0) raw[y-1][x] = WorldEntity.Empty ;
-            if (y +1 < height) raw[y+1][x] = WorldEntity.Empty ;
-
-        }
-        else {
-            //we need to compute a previousDoor
-            x = (int) (Math.random()*width) ; y = (int) (Math.random()*height) ;
-            raw[y][x] = WorldEntity.DoorPrevOpened ;
-        }
-        if (isLastLevel){
-            //we need to generate a Princess
-            while (raw[y][x] != null){ // the computation has to be on an null position
-                x = (int) (Math.random()*width) ; y = (int) (Math.random()*height) ;
-            }
-            raw[y][x] = WorldEntity.Princess ;
-        }
-        else {
-            //we need to generate a nextDoor and a key
-            while (raw[y][x] != null){ // the computation has to be on an null position
-                x = (int) (Math.random()*width) ; y = (int) (Math.random()*height) ;
-            }
-            raw[y][x] = WorldEntity.DoorNextClosed ;
-
-            while (raw[y][x] != null){ // the computation has to be on an empty position
-                x = (int) (Math.random()*width) ; y = (int) (Math.random()*height) ;
-            }
-            raw[y][x] = WorldEntity.Key ;
-        }
-
-        double probas[] = {0.7, 0.07, 0.05, 0.07, 0.02, 0.012, 0.012, 0.015, 0.02, 0.01, 0.02, 0.006} ;
-        String probaNames[] = {"emptyProba", "boxProba", "monsterProba", "stoneProba", "treeProba", "numberIncProba",
-                                "numberDecProba", "rangeIncProba", "rangeDecProba", "heartProba", "landminerProba", "scarecrowProba"} ;
-        WorldEntity entitiesMatches[] = {WorldEntity.Empty, WorldEntity.Box, WorldEntity.Monster, WorldEntity.Stone, WorldEntity.Tree,
-                                        WorldEntity.BombNumberInc, WorldEntity.BombNumberDec, WorldEntity.BombRangeInc, WorldEntity.BombRangeDec,
-                                        WorldEntity.Heart, WorldEntity.Landminer, WorldEntity.BonusScarecrow} ;
-        try (InputStream input = new FileInputStream(new File(path, "config.properties"))) {
-            Properties prop = new Properties();
-            // load the configuration file
-            prop.load(input);
-            //searching for the probabilities on the configuration file
-            for(int i = 0; i < probaNames.length; i++){
-                double tmp = Double.parseDouble(prop.getProperty(probaNames[i], probas[i]+""));
-                probas[i] = tmp ;
-            }
-        } catch (IOException ex) {
-            System.err.println("Error loading configuration");
-        }
-
-        //this part manage the randomisation of the world
-        for(y = 0; y < height; y++){
-            for (x = 0; x < width; x++){
-                if (raw[y][x] == null) {
-                    // empty case
-                    double proba = Math.random() ;
-                    for(int k = 0; k < probaNames.length; k++){
-                        double sum = 0.0 ; //sum is an accumulator which contains all the probas already seen
-                        for(int i = 0 ; i < k ; i++){
-                            sum+=probas[i] ;
-                        }
-                        if (proba < sum + probas[k]){
-                            //proba is between the sum of probas and the sum of probas plus the new proba
-                            raw[y][x] = entitiesMatches[k] ;
-                            break ;
-                        }
-                    }
-                }
-            }
-        }
-        return raw  ;
+        return RandomWorldBuilder.randomBuild(path, level, nb_level_max)  ;
     }
 
     private static Decor processEntity(WorldEntity entity) {
